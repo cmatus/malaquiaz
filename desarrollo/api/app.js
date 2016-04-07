83519013bdbc9185885c756d9995d64e32b71dcd
Claudio Matus <claudio.matus@local.dts.cl>
2016-04-07 10:54:37 -0300

    2016-04-07

---
+++ desarrollo/api/app.js
@@ -0,0 +1,53 @@
+var http = require('http');
+var express = require('express');
+var path = require('path');
+
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var methodOverride = require('method-override');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var errorHandler = require('errorhandler');
+var mongoose = require('mongoose');
+var cors = require('cors');
+var app = express();
+
+mongoose.connect('mongodb://localhost/pizzeria');
+
+app.set('port', process.env.PORT || 3000);
+app.use(logger('dev'));
+app.use(methodOverride());
+app.use(session({ resave: true,
+                  saveUninitialized: true,
+                  secret: 'uwotm8' }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(multer());
+
+app.use(cors({ origin: 'http://localhost:3000/api' }));
+
+app.all('*', function(req, res, next) {
+
+    var responseSettings = {
+        "AccessControlAllowOrigin": req.headers.origin,
+        "AccessControlAllowHeaders": "Content-Type,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name",
+        "AccessControlAllowMethods": "POST, GET, PUT, DELETE",
+        "AccessControlAllowCredentials": true
+    };
+
+    res.header("Access-Control-Allow-Credentials", responseSettings.AccessControlAllowCredentials);
+    res.header("Access-Control-Allow-Origin",  responseSettings.AccessControlAllowOrigin);
+    res.header("Access-Control-Allow-Headers", (req.headers['access-control-request-headers']) ? req.headers['access-control-request-headers'] : "x-requested-with");
+    res.header("Access-Control-Allow-Methods", (req.headers['access-control-request-method']) ? req.headers['access-control-request-method'] : responseSettings.AccessControlAllowMethods);
+
+});
+
+if ('development' == app.get('env')) {
+    app.use(errorHandler());
+}
+
+var server = http.createServer(app);
+server.listen(app.get('port'), function(){
+    console.log('Corriendo por puerto: ' + app.get('port'));
+});
